ab0748bb11223170d3a2f5d1310147399b4ce056
David Melo <davidbezerra10@hotmail.com>
2024-03-07 15:10:42 -0300

    Voltando a versão anterior sem o back-end para depois refazê-lo utilizando node.js

--- app_login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('cadastrar_usu/', views.cadastrar_usu, name='cadastrar_usu'),
-    path('login_user/', views.login_user, name='login_user')
-]
